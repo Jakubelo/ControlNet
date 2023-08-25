e42b6ac9cb420cf745b1c7adafd33bb14cae8764
Jakub Galik <jakub.galik@digica.com>
2023-08-25 23:32:54 +0200

    inference in simple script

---
+++ inference_sd21.py
@@ -0,0 +1,41 @@
+from share import *
+
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from s2s_dataset import PredictS2sDataSet
+from cldm.logger import ImageLogger
+from cldm.model import create_model, load_state_dict
+from pathlib import Path
+
+
+# Configs
+resume_path = 'models/control_sd21_ini.ckpt'
+input_directory = Path('/home/jgalik/repos/ControlNet/dataset/preprocessed/test2/')
+batch_size = 2
+logger_freq = 300
+learning_rate = 1e-5
+sd_locked = True
+only_mid_control = False
+size = 512, 512
+
+
+# First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
+model = create_model('./models/cldm_v21.yaml').cpu()
+model.load_state_dict(load_state_dict(resume_path, location='cpu'))
+breakpoint()
+model.eval()
+# model.learning_rate = learning_rate
+# model.sd_locked = sd_locked
+model.only_mid_control = only_mid_control
+
+
+# Misc
+dataset = PredictS2sDataSet(input_directory, size)
+dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
+logger = ImageLogger(batch_frequency=logger_freq)
+trainer = pl.Trainer(accelerator='gpu', precision=32, callbacks=[logger])
+
+
+# Train!
+results = trainer.predict(model, dataloader)
+breakpoint()
